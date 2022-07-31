a5ee5f370a6d12d01d4daca0fe18786a67ec49a9
ZaidCrammax <zaidakhtar102@gmail.com>
2022-07-31 12:06:35 +0000

    identified first bug

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -11,6 +11,19 @@ from cocotb.clock import Clock
 
 from model_mkbitmanip import *
 
+mav_putvalue_src1 = 0x5
+mav_putvalue_src2 = 0x0
+mav_putvalue_src3 = 0x0
+   
+func7 = [0b0100000, 0b0010000, 0b0110000, 0b0100100, 0b0010100, 0b0110100, 0b0000101, 0b0000100]
+func3 = [0b111, 0b110, 0b100, 0b001, 0b101, 0b010, 0b011]
+opcode = [0b0110011, 0b0010011]
+func7_imm = [0b00100, 0b01100, 0b01001, 0b00101, 0b01101]
+func7_2bit = [0b11, 0b10]
+imm_value_1 = [0b00000, 0b00001, 0b00010, 0b00100, 0b10000, 0b10001, 0b10010, 0b11000, 0b11001, 11010]
+func7_imm_SHFL = 0b000010
+func7_fsri_1bit = 0b1
+
 # Clock Generation
 @cocotb.coroutine
 def clock_gen(signal):
@@ -22,7 +35,7 @@ def clock_gen(signal):
 
 # Sample Test
 @cocotb.test()
-def run_test(dut):
+def run_test0(dut):
 
     # clock
     cocotb.fork(clock_gen(dut.CLK))
@@ -34,10 +47,9 @@ def run_test(dut):
 
     ######### CTB : Modify the test to expose the bug #############
     # input transaction
-    mav_putvalue_src1 = 0x5
-    mav_putvalue_src2 = 0x0
-    mav_putvalue_src3 = 0x0
-    mav_putvalue_instr = 0x101010B3
+    
+
+    mav_putvalue_instr = ((func7[0])<<25) | ((func3[0])<<12) | opcode[0]
 
     # expected output from the model
     expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
@@ -48,7 +60,47 @@ def run_test(dut):
     dut.mav_putvalue_src3.value = mav_putvalue_src3
     dut.EN_mav_putvalue.value = 1
     dut.mav_putvalue_instr.value = mav_putvalue_instr
-  
+    
+    yield Timer(1) 
+
+    # obtaining the output
+    dut_output = dut.mav_putvalue.value
+
+    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
+    cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+    
+    # comparison
+    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
+    assert dut_output == expected_mav_putvalue, error_message
+
+
+@cocotb.test()
+def run_test1(dut):
+
+    # clock
+    cocotb.fork(clock_gen(dut.CLK))
+
+    # reset
+    dut.RST_N.value <= 0
+    yield Timer(10) 
+    dut.RST_N.value <= 1
+
+    ######### CTB : Modify the test to expose the bug #############
+    # input transaction
+    
+
+    mav_putvalue_instr = ((func7[0])<<25) | ((func3[0])<<12) | opcode[0]
+
+    # expected output from the model
+    expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+
+    # driving the input transaction
+    dut.mav_putvalue_src1.value = mav_putvalue_src1
+    dut.mav_putvalue_src2.value = mav_putvalue_src2
+    dut.mav_putvalue_src3.value = mav_putvalue_src3
+    dut.EN_mav_putvalue.value = 1
+    dut.mav_putvalue_instr.value = mav_putvalue_instr
+    
     yield Timer(1) 
 
     # obtaining the output
